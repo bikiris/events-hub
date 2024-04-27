43535ea4dd1956959126c70a326cfeccefa1c146
bikiris <a664347635@gmail.com>
2024-04-26 21:48:32 -0400

    Add img

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -2,13 +2,17 @@ function MainPage() {
     return (
       <>
         <div className="w-full relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-6 flex items-center lg:px-8">
             <div className="mx-auto max-w-2xl lg:mx-0">
               <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Meet new people sharing your interests</h2>
               <p className="mt-6 text-lg leading-8 text-gray-300">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
                 fugiat veniam occaecat fugiat aliqua.
               </p>
+            </div>
+
+            <div className="mx-auto max-w-2xl lg:mx-0">
+              <img src="https://cdn.pixabay.com/photo/2018/03/21/06/30/community-3245739_960_720.png" alt="People meeting" />
             </div>
           </div>
         </div>
